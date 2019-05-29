df94d4dbf4eb7f365834d04256c5545844a632bc
Ethan <wang197200@163.com>
2019-05-29 17:25:49 +0800

    init

---
+++ EWImageTapBig/EWImageAmplification.swift
@@ -0,0 +1,53 @@
+//
+//  EWImageAmplification.swift
+//  EWImageTapBig
+//
+//  Created by Ethan.Wang on 2019/5/29.
+//  Copyright © 2019 王利权. All rights reserved.
+//
+import UIKit
+
+var oldFrame: CGRect = CGRect()
+
+let EWidth = UIScreen.main.bounds.width
+let EHeight = UIScreen.main.bounds.height
+
+class EWImageAmplification: NSObject {
+    private let backGroundView = UIView(frame: CGRect(x: 0, y: 0, width: EWidth, height: EHeight))
+    
+    private let imageView = UIImageView(frame: oldFrame)
+
+    func scanBigImageWithImageView(currentImageView: UIImageView, alpha: CGFloat){
+        let image = currentImageView.image
+        let window = UIApplication.shared.keyWindow
+        
+        oldFrame = currentImageView.convert(currentImageView.bounds, to: window)
+        backGroundView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: alpha)
+        backGroundView.alpha = 0
+        
+        imageView.image = image
+        imageView.contentMode = .scaleAspectFit
+        backGroundView.addSubview(imageView)
+        backGroundView.isUserInteractionEnabled = true
+        window?.addSubview(backGroundView)
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapOnBackView(_:)))
+        backGroundView.addGestureRecognizer(tap)
+        
+        UIView.animate(withDuration: 0.4) { [weak self] in
+            let y: CGFloat = (EHeight - (image?.size.height)! * EWidth / (image?.size.width)!) * 0.5
+            let height = (image?.size.height)! * EWidth / (image?.size.width)!
+            self?.imageView.frame = CGRect(x: 0, y: y, width: EWidth, height: height)
+            self?.backGroundView.alpha = 1
+        }
+    }
+    
+    @objc private func tapOnBackView(_ sender: UITapGestureRecognizer){
+        UIView.animate(withDuration: 0.4, animations: { [weak self] in
+            self?.imageView.frame = oldFrame
+            self?.backGroundView.alpha = 0
+        }, completion: { [weak self] (finished) in
+            self?.backGroundView.removeFromSuperview()
+        })
+    }
+}
--- EWImageTapBig/ViewController.swift
+++ EWImageTapBig/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let imageView: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: (EWidth - 200) / 2, y: 250, width: 200, height: 200))
+        imageView.backgroundColor = UIColor.gray
+        imageView.isUserInteractionEnabled = true
+        imageView.image = UIImage(named: "back")
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        drawMyView()
     }
-
+    private func drawMyView(){
+        self.view.addSubview(imageView)
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapOnImageView))
+        imageView.addGestureRecognizer(tap)
+    }
+    @objc private func tapOnImageView(){
+        EWImageAmplification().scanBigImageWithImageView(currentImageView: imageView, alpha: 1)
+    }
 
 }
 
