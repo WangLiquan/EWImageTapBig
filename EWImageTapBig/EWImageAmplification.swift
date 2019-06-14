afc6f02e583e1b3234d024363f8aaf69fa6ad79b
Ethan <wang197200@163.com>
2019-06-14 14:14:28 +0800

    Update EWImageAmplification.swift
    添加注释

--- EWImageTapBig/EWImageAmplification.swift
+++ EWImageTapBig/EWImageAmplification.swift
@@ -22,8 +22,10 @@ class EWImageAmplification: NSObject {
     ///   - alpha: 背景透明度(0-1)
     func scanBigImageWithImageView(currentImageView: UIImageView, alpha: CGFloat) {
         let image = currentImageView.image
+        /// 获取keyWindow
         let window = UIApplication.shared.keyWindow
         let backGroundView = UIView(frame: CGRect(x: 0, y: 0, width: EWidth, height: EHeight))
+        /// 记录imageView初始frame，保证复原
         oldFrame = currentImageView.convert(currentImageView.bounds, to: window)
         let imageView = UIImageView(frame: oldFrame)
         backGroundView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: alpha)
@@ -34,12 +36,12 @@ class EWImageAmplification: NSObject {
         backGroundView.addSubview(imageView)
         backGroundView.isUserInteractionEnabled = true
         window?.addSubview(backGroundView)
-
+        /// 为背景View添加手势方法，实现点击复原
         let tap: EWTap = EWTap(target: self, action: #selector(tapOnBackView(_:)))
         tap.backView = backGroundView
         tap.imageView = imageView
         backGroundView.addGestureRecognizer(tap)
-
+        /// 实现动画效果
         UIView.animate(withDuration: 0.4) {
             let imageY: CGFloat = (EHeight - (image?.size.height)! * EWidth / (image?.size.width)!) * 0.5
             let height = (image?.size.height)! * EWidth / (image?.size.width)!
@@ -47,7 +49,10 @@ class EWImageAmplification: NSObject {
             backGroundView.alpha = 1
         }
     }
-
+    
+    /// 点击背景手势方法
+    ///
+    /// - Parameter sender: 手势
     @objc private func tapOnBackView(_ sender: EWTap) {
         UIView.animate(withDuration: 0.4, animations: {
             sender.imageView?.frame = oldFrame
@@ -57,7 +62,7 @@ class EWImageAmplification: NSObject {
         })
     }
 }
-
+/// 自定义手势类，实现手势方法传参
 class EWTap: UITapGestureRecognizer {
     var backView: UIView?
     var imageView: UIImageView?
